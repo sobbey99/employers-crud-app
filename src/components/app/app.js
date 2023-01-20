de25d95ab1355e5be763f76057e21ef29208853a
sobbey99 <sobbey99@gmail.com>
2023-01-20 13:33:29 +0200

    Added Props for List Items

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -6,6 +6,13 @@ import EmployersAddForm from '../emplyers-add-form/employers-add-form';
 import './app.css';
 
 function App() {
+
+    const data = [
+        {name: 'John C.' ,salary: 800, increase: false },
+        {name: 'Alex M.' ,salary: 3000, increase: true },
+        {name: 'Carl W.' ,salary: 15000, increase: false },
+    ];
+
     return (
         <div className="app">
             <AppInfo/>
@@ -15,7 +22,7 @@ function App() {
                 <AppFilter />
             </div>
 
-            <EmployersList />
+            <EmployersList data={data}/>
 
             <EmployersAddForm />
         </div>

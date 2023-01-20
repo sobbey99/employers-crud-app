364e2b1345add233182bea62c96786cd92d2d54f
sobbey99 <sobbey99@gmail.com>
2023-01-20 14:32:36 +0200

    Generating list from array and added propsm and keys

--- src/components/employers-list/employers-list.js
+++ src/components/employers-list/employers-list.js
@@ -5,10 +5,12 @@ import './employers-list.css';
 const EmployersList = ({data}) => {
 
     const elements = data.map(item => {
+        const {id, ...itemProps} = item;
         return (
-            <EmployersListItem {...item}/>
+            <EmployersListItem key={id} {...itemProps}/>
         )
     })
+
 
     return (
         <ul className="app-list list-group">

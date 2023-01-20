ef40bf44b675e9b66cb518349fa61267885c4f54
sobbey99 <sobbey99@gmail.com>
2023-01-20 17:29:08 +0200

    Delete function added in class App

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,32 +1,60 @@
+import { Component } from 'react';
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
 import EmployersList from '../employers-list/employers-list';
 import EmployersAddForm from '../emplyers-add-form/employers-add-form';
+
 import './app.css';
 
-function App() {
-
-    const data = [
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
         {name: 'John C.' ,salary: 800, increase: false ,  id: 1},
         {name: 'Alex M.' ,salary: 3000, increase: true ,  id: 2},
         {name: 'Carl W.' ,salary: 15000, increase: false, id: 3},
-    ];
+        {name: 'Mike F.' ,salary: 1000, increase: false,  id: 4},
+            ]
+        }
+    }
 
-    return (
-        <div className="app">
-            <AppInfo/>
+    deleteItem = (id) => {
+        this.setState(({data}) => {
+            // const index = data.findIndex(elem => elem.id === id);
+            //METHOD 1 for copy state
+            // const before = data.slice(0, index);
+            // const after = data.slice(index + 1);
+            // const newArr = [...before, ...after];
 
-            <div className="search-panel">
-                <SearchPanel/>
-                <AppFilter />
+
+            return {
+                //METHOD 2 for copy state
+                data: data.filter(item => item.id !== id)
+            }
+
+        })
+    }
+
+    render() {
+        return (
+            <div className="app">
+                <AppInfo/>
+    
+                <div className="search-panel">
+                    <SearchPanel/>
+                    <AppFilter />
+                </div>
+    
+                <EmployersList data={this.state.data}
+                onDelete={this.deleteItem}/>
+    
+                <EmployersAddForm />
             </div>
-
-            <EmployersList data={data}/>
-
-            <EmployersAddForm />
-        </div>
-    );
+        );
+    }
 }
 
 export default App;
--- src/components/employers-list-item/employers-list-item.js
+++ src/components/employers-list-item/employers-list-item.js
@@ -24,7 +24,7 @@ class EmployersListItem extends Component{
     }
 
     render() {
-        const {name, salary} = this.props;
+        const {name, salary, onDelete} = this.props;
         const {increase} = this.state;
         const {rise} = this.state;
         let classNames = 'list-group-item d-flex justify-content-between';
@@ -50,7 +50,8 @@ class EmployersListItem extends Component{
             </button>
 
             <button type="button"
-                    className="btn-trash btn-sm ">
+                    className="btn-trash btn-sm"
+                    onClick={onDelete}>
                 <i className="fas fa-trash"></i>
             </button>
             <i className="fas fa-star"></i>

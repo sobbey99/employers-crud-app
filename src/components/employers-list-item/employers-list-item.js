d021cb09239ae9cd758fa9e460408a8995177b8b
sobbey99 <sobbey99@gmail.com>
2023-01-20 16:05:56 +0200

    Added State object with increase and rise classes onClick

--- src/components/employers-list-item/employers-list-item.js
+++ src/components/employers-list-item/employers-list-item.js
@@ -1,20 +1,51 @@
+import {Component} from 'react';
 import "./employers-list-item.css";
 
 
-const EmployersListItem = ({name, salary, increase}) => {
-
-    let classNames = 'list-group-item d-flex justify-content-between';
-    if (increase) {
-        classNames += ' increase'
+class EmployersListItem extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            increase: false,
+            rise: false
+        }
     }
+
+    onIncrease = () => {
+        this.setState(({increase}) => ({
+            increase: !increase
+        }))
+    }
+
+    onRise = () => {
+        this.setState(({rise}) => ({
+            rise: !rise
+        }))
+    }
+
+    render() {
+        const {name, salary} = this.props;
+        const {increase} = this.state;
+        const {rise} = this.state;
+        let classNames = 'list-group-item d-flex justify-content-between';
+        if(rise) {
+            classNames += ' like'
+        }
+
+
+        if (increase) {
+        classNames += ' increase'
+        }
 
     return (
     <li className={classNames}>
-        <span className="list-group-item-label">{name}</span>
+        <span className="list-group-item-label"
+            onClick={this.onRise}>{name}</span>
         <input type="text" className="list-group-item-input" defaultValue={salary + '$'}/>
         <div className='d-flex justify-content-center align-items-center'>
             <button type="button"
-                className="btn-cookie btn-sm ">
+                className="btn-cookie btn-sm "
+                onClick={this.onIncrease}>
                 <i className="fas fa-cookie"></i>
             </button>
 
@@ -26,6 +57,7 @@ const EmployersListItem = ({name, salary, increase}) => {
         </div>
     </li>
     )
+    }
 }
 
 export default EmployersListItem;
